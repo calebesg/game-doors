9f70cfecaa513484fbd8f73e2299da73888806a0
CalebeSG <calebeguimaraes10@gmail.com>
2022-06-20 10:26:02 -0300

    create doors array

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,30 @@
 import { useState } from "react";
 import Door from "../components/Door";
+import { createDoors, updateDoors } from "../functions/doors";
 import DoorModel from "../models/DoorModel";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const [door, setDoor] = useState(new DoorModel(1));
+  const [doors, setDoors] = useState(createDoors(3, 2));
+
+  const renderDoors = function () {
+    return doors.map((door) => {
+      return (
+        <Door
+          key={door.number}
+          value={door}
+          onChange={(newDoor) => setDoors(updateDoors(doors, newDoor))}
+        />
+      );
+    });
+  };
 
   return (
     <main
       className={styles.main}
       style={{ display: "flex", gap: "1rem", flexDirection: "row" }}
     >
-      <Door value={door} onChange={(newDoor) => setDoor(newDoor)} />
+      {renderDoors()}
     </main>
   );
 }
