ba40a8e51644df01fa760d976588b4a3b485f447
CalebeSG <calebeguimaraes10@gmail.com>
2022-06-21 09:46:45 -0300

    create card component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
+import styles from "../styles/pages/Home.module.css";
+import Card from "../components/Card";
+import Link from "../../node_modules/next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Bem Vindo ao game</h1>
-    </div>
+    <main className={styles.container}>
+      <div className={styles.menu}>
+        <Card bgColor="rgb(255, 56, 83)">
+          <h1 className={styles.title}>Monty Holl</h1>
+        </Card>
+        <Card></Card>
+        <Card></Card>
+        <Card bgColor="rgb(56, 255, 162)">
+          <Link href={`/game/5/2`}>
+            <h2 className={styles.link}>Iniciar</h2>
+          </Link>
+        </Card>
+      </div>
+    </main>
   );
 }
